704f9d21dc94cbe1884d1aa7bc89eae27ad8d405
Sobirjon <151761986+abdurasulovsobirjon1120@users.noreply.github.com>
2025-04-02 11:40:41 +0500

    Update settings.py

--- myp/settings.py
+++ myp/settings.py
@@ -16,7 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR sozlamasi
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
